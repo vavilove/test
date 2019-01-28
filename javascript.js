e594570b587f1cd6589351c2ee31d41558bcc0f5
Aleksandr <tos-os@mail.ru>
2019-01-28 14:09:31 +0300

    это второй коммит домащнее задание

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
 "use strict"
 
-let money = prompt("Ваш бюджет на месяц?", ''),
+var money = +prompt("Ваш бюджет на месяц?", ''),
     time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
-let appData = {
+var appData = {
     budget: money,
     timeData: time,
     expenses: {},
@@ -12,12 +12,31 @@ let appData = {
     savings: false
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    a2 = prompt("Во сколько обойдется", '');
 
-    expenses: {
-        a1 : a2
-        };
 
-alert(appData.budget / 30);
-  
+for (let i=0; i<2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+            b = prompt("Во сколько обойдется", '');
+    if  ( (typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) != null 
+    && a != '' && b != '' && a.length < 50) {
+        console.log ('done');
+        appData.expenses[a] = b;   
+    } else {
+        console.log ('what');
+        i--;
+    }  
+}
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Ежедневный бюджет" + appData.moneyPerDay );
+
+if(appData.moneyPerDay < 100) {
+    console.log("У вас низкий уровень достатка");
+} else if(appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("У вас средний уровень достатка");
+} else if(appData.moneyPerDay > 2000) {
+    console.log("У вас высокий уровень достатка");
+} else {
+    console.log("ошибка");
+}
