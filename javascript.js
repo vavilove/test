3e3d5137ab30a49594ce019cf3829ecb822b5ac6
Aleksandr <tos-os@mail.ru>
2019-02-01 07:06:39 +0300

    Эо третий коммит

--- javascript.js
+++ javascript.js
@@ -1,7 +1,15 @@
 "use strict"
 
-var money = +prompt("Ваш бюджет на месяц?", ''),
+let money, time;
+
+function start() {
+    money = +prompt("Ваш бюджет на месяц?", '');
     time = prompt("Введите дату в формате YYYY-MM-DD", '');
+    while(isNaN(money) || money =='' || money == null) {
+        money = +prompt("Ваш бюджет на месяц?", '');
+    }
+}
+start();
 
 var appData = {
     budget: money,
@@ -9,12 +17,11 @@ var appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true
 };
 
-
-
-for (let i=0; i<2; i++) {
+function chooseExpenses() {
+    for (let i=0; i<2; i++) {
         let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
             b = prompt("Во сколько обойдется", '');
     if  ( (typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) != null 
@@ -26,8 +33,11 @@ for (let i=0; i<2; i++) {
         i--;
     }  
 }
+}
+chooseExpenses();
 
-appData.moneyPerDay = appData.budget / 30;
+
+appData.moneyPerDay = (appData.budget / 30).toFixed();
 
 alert("Ежедневный бюджет" + appData.moneyPerDay );
 
@@ -40,3 +50,14 @@ if(appData.moneyPerDay < 100) {
 } else {
     console.log("ошибка");
 }
+
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt("Какова сумма накоплений?"),
+            persent = +prompt ("Под какой процент?");
+
+            appData.monthIncome = save/100/12*persent;
+            alert ("Доход с Вашего депозита: " + appData.monthIncome);
+    }
+}
+checkSavings();
